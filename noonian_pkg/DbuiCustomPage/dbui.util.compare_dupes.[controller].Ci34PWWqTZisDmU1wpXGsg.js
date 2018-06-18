81a936c3a6fd9a7b3fedb9fcd5dec261f12d4a79
Gene <gene@noonian.org>
2018-06-18 11:27:27 -0700

    Improved record-merge UI

--- noonian_pkg/DbuiCustomPage/dbui.util.compare_dupes.[controller].Ci34PWWqTZisDmU1wpXGsg.js
+++ noonian_pkg/DbuiCustomPage/dbui.util.compare_dupes.[controller].Ci34PWWqTZisDmU1wpXGsg.js
@@ -1,65 +1,110 @@
 function (db, $scope, $stateParams, $q, $timeout, NoonWebService, DbuiFieldType, DbuiAlert) {
     
+    var bgColors = ['#004586','#ffd320','#ff420e','#579d1c','#7e0021','#83caff','#314004'];
+    var fgColors = ['white','black','black','black','white','black','white'];
+    
+    
     var dupeListId = $stateParams.id;
+    // console.log($stateParams);
     
-    if(!dupeListId) {
-        window.history.back();
-        return;
-    }
+    var className = $scope.className = $stateParams.className || ($stateParams.extraParams && $stateParams.extraParams.className);
+    var perspectiveName = $stateParams.perspective || 'default';
     
-    $scope.formStatusA = {};
-    $scope.formStatusB = {};
-    var theDupeList;
-    var theDupeSpec;
-    
-    var TargetModel;
-    var tdMap;
+    var TargetModel = db[className];
+    var tdMap = $scope.typeDescMap = TargetModel._bo_meta_data.type_desc_map;
     var fieldList;
     
-    db.DupeList.findOne({_id:dupeListId}).$promise.then(function(dupeList) {
-        theDupeList = dupeList;
-        
-        //Grab the spec
-        return db.DupeSpec.findOne({_id:dupeList.spec._id}).$promise;
-    })
-    .then(function(dupeSpec) {
-        theDupeSpec = dupeSpec;
-        TargetModel = db[theDupeSpec.class._disp];
-        tdMap = TargetModel._bo_meta_data.type_desc_map;
-        
-        var refIds = _.pluck(theDupeList.dups, '_id'); //pluck out the _id's into an array
-        
-        
-        
-        return $q.all([
-            Dbui.getPerspective('diff-compare', theDupeSpec.class._disp, 'edit'),
-            TargetModel.findOne({_id:theDupeList.orig._id}).$promise,
-            TargetModel.find({_id:{$in:refIds}}).$promise,
-            DbuiFieldType.cacheTypeInfoForClass(theDupeSpec.class._disp, 'edit')
-        ]);
-    })
-    .then(function(resultArr) {
-        
-        $scope.editPerspective = resultArr[0];
-        var origObj = resultArr[1];
-        var dupObjArr = resultArr[2];
-        
-        $scope.objectA = origObj;
-        $scope.objectB = dupObjArr[0];
-        
-        $scope.allObjects = [];
-        
-        var len=0;
-        _.forEach(dupObjArr, function(o) {
-            len++;
-            $scope.allObjects.push({
-                label:len+'. '+o._disp+' ('+o._id+')',
-                obj:o
-            });
+    //Stuff for merge pane:
+    $scope.labelGroup = TargetModel._bo_meta_data.field_labels;
+    $scope.colClass = Dbui.columnClasses;
+    $scope.linkStatus = {};
+    // $scope.$watch('linkStatus', function(ls) {
+    //     $scope.editorForm.$setPristine();
+    //     if($scope.formStatus) $scope.formStatus.isDirty = false;
+    // }, true);
+    
+    
+    const tabStatus = $scope.tabStatus = {activeTab:'A'};
+    const compTabs = $scope.compTabs = [
+        {index:'A',bgColor:bgColors[0],fgColor:fgColors[0]},
+        {index:'B',bgColor:bgColors[1],fgColor:fgColors[1]}
+    ];
+    var tabIndex = _.indexBy(compTabs, 'index');
+    var tabsByObjectId;
+    
+    const updateFilter = function() {
+        var selected = [];
+        _.forEach(compTabs, t => {
+            if(t.id) {
+                selected.push({_id:{$ne:t.id}});
+            }
         });
         
+        var query = null;
+        if(selected.length == 1) {
+            query = selected[0];
+        }
+        else {
+            query = {$and:selected};
+        }
+        // console.log('setting query', query);
+        
+        $scope.listPerspective.filter = query;
+        $scope.refreshForms();
+    }
+    
+    const checkMergeReady = function() {
+        var anyEmpty = false;
+        _.forEach(compTabs, t => {
+            anyEmpty = anyEmpty || !t.object;
+        });
+        
+        if(!$scope.mergeReady && !anyEmpty) {
+            tabStatus.activeTab = 3;
+            //Initialize the merge!
+            $scope.mergedObj = new TargetModel();
+            $scope.mergeStatus = {};
+            
+            _.assign($scope.mergedObj, compTabs[0].object);
+            
+            tabsByObjectId = _.indexBy(compTabs, 'object._id');
+            
+            $scope.autoMerge(compTabs[1].object, $scope.mergedObj);
+            
+            tabStatus.activeTab = 'merged';
+            $scope.mergeReady = true;
+        }
+        
+        
+        
+    }
+    
+    const selectListItem = function(selection) {
+        // console.log(tabStatus);
+        // console.log(selection.targetObj);
+        var selectedObj = selection.targetObj;
+        var myTab = tabIndex[tabStatus.activeTab];
+        myTab.id = selectedObj._id;
+        myTab.object = TargetModel.findOne({_id:selectedObj._id});
+        updateFilter();
+        myTab.object.$promise.then(checkMergeReady);
+    };
+    
+    
+    
+    $q.all([
+        Dbui.getPerspective(perspectiveName, className, 'diff-compare'),
+        DbuiFieldType.cacheTypeInfoForClass(className, 'edit')
+    ])
+    .then(function([perspective]) {
+        // console.log(perspective);
+        $scope.editPerspective = perspective;
+        $scope.listPerspective = perspective;
+        
+        perspective.recordActions = ['dialog-view', {icon:'fa-check-square', fn:selectListItem}];
+        
         fieldList = [];
-        _.forEach($scope.editPerspective.layout, function(sec) {
+        _.forEach(perspective.layout, function(sec) {
             _.forEach(sec.rows, function(row) {
                 _.forEach(row, function(f) {
                     fieldList.push(f);
@@ -67,197 +112,229 @@ function (db, $scope, $stateParams, $q, $timeout, NoonWebService, DbuiFieldType,
             });
         });
         
-    })
+    },
+    function(err) {
+        console.error('retreiving perspectives', err);
+    }
+    )
     ;
     
-    $scope.dedup = function() {
-       NoonWebService.call('dupehelper/dedup', {
-           boClass: TargetModel._bo_meta_data.class_name,
-           duplicateId:  $scope.objectB._id,
-           originalId:  $scope.objectA._id
-        })
-        .then(function(wsResult) {
-            DbuiAlert.success(wsResult.message);
+    
+    
+    
+    
+    $scope.commitMerge = function() {
+        console.log(compTabs);
+        console.log('$scope.mergedObj', $scope.mergedObj);
         
-            if($scope.allObjects.length === 1) {
-                theDupeList.remove().then(function() {
-                    $state.go(
-                        'dbui.list', 
-                        {
-                            className: 'DupeList',
-                            perspective:'default'
-                        }
-                    );
-                });
-            }
-            else {
-                var objs = $scope.allObjects;
-                for(var i=0; i < objs.length; i++) {
-                    if(objs[i] === $scope.objectB) {
-                        objs.splice(i, 1);
-                        break;
-                    }
-                }
-                $scope.objectB = objs[0];
-                $scope.refreshForms();
-            }
+        $scope.mergedObj.save().then(function(m) {
+            DbuiAlert.success('Successfully saved '+m._disp);
+            var dup = compTabs[1].object;
+            
+            
+            return NoonWebService.call('dupehelper/dedup', {
+              boClass: className,
+              duplicateId:  dup._id,
+              originalId:  m._id
+            })
+            .then(function(wsResult) {
+                DbuiAlert.success(wsResult.message);
+                $scope.resultMessage = wsResult.message;
+                $scope.mergeComplete = true;
+            });
+        },
+        function(err) {
+            console.log('error saving merged', err);
+            DbuiAlert.danger(err);
         });
         
-    };
-    
-    var markDiff = function(f) {
-        var selector = '.control-label[for="'+f+'"]';
-        $(selector).css('background-color', 'red'); 
-    };
-    
-    var clearDiff = function(f) {
-        var selector = '.control-label[for="'+f+'"]';
-        $(selector).css('background-color', ''); 
-    };
-    
-    
-    var labelClicked = function(e) {
-        var clicked = $(e.target);
-        var fieldName = clicked.attr('for');
-        var whichContainer = clicked.closest('.objContainer');
-        var whichContainerId = whichContainer.attr('id');
-        var srcObj = whichContainerId ==='Acontainer' ? $scope.objectA : $scope.objectB;
-        var destObj = srcObj === $scope.objectA ? $scope.objectB : $scope.objectA;
-        
-        // console.log(fieldName, srcObj, destObj);
-        
-        var td = tdMap.getTypeDescriptor(fieldName);
-        if(td.type === 'text' && destObj[fieldName]) {
-            destObj[fieldName] += '\n'+srcObj[fieldName];
-        }
-        else {
-            destObj[fieldName] = srcObj[fieldName];
-        }
-        
-        clearDiff(fieldName);
+       
         
     };
-    var wiredup = false;
-    $scope.showDiffs = function() {
-        $('.control-label').css('background-color', '');
-        if(!wiredup) {
-            $('.control-label').on('click', labelClicked);
-            wiredup=true;
-        }
-        
-        // console.log($scope.editPerspective);
-        var objectA = $scope.objectA;
-        var objectB = $scope.objectB;
-        // console.log('showDiffs', $scope, objectA, objectB);
-        
-        _.forEach(fieldList, function(f) {
-            if(!angular.equals(objectA[f], objectB[f])) {
-                markDiff(f);
-            }
-        });
-    };
+
     
-    var mergeText = function(a, b) {
+    
+    
+    var mergeText = function(to, from, field, mergeStatus) {
+        var a = to[field];
+        var b = from[field];
+        
         if(a === b) {
-            return a;
+            mergeStatus[field] = {status:'identical'};
         }
         else if(a && b) {
-            return a+'\n'+b;
+            mergeStatus[field] = {status:'combined'};
+            to[field] = a+'\n'+b;
         }
         else if(a) {
-            return a;
+            mergeStatus[field] = {
+                status:'one_empty',
+                source:tabsByObjectId[to._id].index
+            };
         }
         else {
-            return b;
+            mergeStatus[field] = {
+                status:'one_empty',
+                source:tabsByObjectId[from._id].index
+            };
+            
+            to[field] = b;
         }
     };
     
-    var mergeArray = function(arrA, arrB) {
+    var mergeArray = function(to, from, field, mergeStatus) {
+        var arrA = to[field] || [];
+        var arrB = from[field] || [];
+        
+        if(!arrA.length && !arrB.length) {
+            mergeStatus[field] = {status:'identical'};
+            return;
+        }
+        else if(arrA.length && !arrB.length) {
+            mergeStatus[field] = {
+                status:'one_empty',
+                source:tabsByObjectId[to._id].index
+            };
+            return;
+        }
+        else if(!arrA.length && arrB.length) {
+            mergeStatus[field] = {
+                status:'one_empty',
+                source:tabsByObjectId[from._id].index
+            };
+            to[field] = arrB;
+            return;
+        }
+        
+        var anyChange = false;
         //add each of b into a if not already there
         for(var i = 0; i < arrB.length; i++) {
             var found = false;
             for(var j=0; j < arrA.length; j++) {
                 var a = arrA[j], b = arrB[i];
                 if(!a || !b) continue;
-                if(a === b || a._id === b._id) {
+                if(a === b || a._id === b._id || angular.equals(a, b)) {
                     found = true;
                     break;
                 }
             }
             if(!found) {
                 arrA.push(arrB[i]);
+                anyChange = true;
             }
+        }
+        
+        if(anyChange) {
+            mergeStatus[field] = {status:'combined'};
+        }
+        else {
+            mergeStatus[field] = {status:'identical'};
         }
     };
     
     $scope.autoMerge = function(from, to) {
+        var mergeStatus = $scope.mergeStatus;
+        
         _.forEach(fieldList, function(f) {
             var td = tdMap.getTypeDescriptor(f);
+            
+            
         
             if(td.type === 'text') {
-                to[f] = mergeText(to[f], from[f]);
+                mergeText(to, from, f, mergeStatus);
+            }
+            else if(from[f] instanceof Array && to[f] instanceof Array) {
+                mergeArray(to, from, f, mergeStatus);
+            }
+            else if((to[f] == null || to[f] == '') && (from[f] == null || from[f] == '')) {
+                mergeStatus[f] = {status:'identical'};
+            }
+            else if(angular.equals(to[f], from[f])) {
+                mergeStatus[f] = {status:'identical'};
             }
             else if((to[f] == null || to[f] == '') && from[f] != null) {
                 to[f] = from[f];
+                mergeStatus[f] = {
+                    status:'one_empty',
+                    source:tabsByObjectId[from._id].index
+                };
             }
-            else if(from[f] instanceof Array && to[f] instanceof Array) {
-                if(from[f].length) {
-                    mergeArray(to[f], from[f]);
-                }
+            else if((from[f] == null || from[f] == '') && to[f] != null) {
+                mergeStatus[f] = {
+                    status:'one_empty',
+                    source:tabsByObjectId[to._id].index
+                };
+            }
+            else {
+                //both not null, and not equal
+                mergeStatus[f] = {
+                    status:'manual_merge_required',
+                    source:tabsByObjectId[to._id].index,
+                    sourceTab:tabsByObjectId[to._id]
+                };
             }
         });
-        $scope.showDiffs();
+        
     };
-     $scope.save = function(obj, fs) {
-         console.log('save', obj);
-         obj.save().then(function() {
-             fs.isDirty = false;
-         });
-     };
+    
+    var statusToClass = {
+        manual_merge_required:'bg-danger',
+        one_empty:'bg-success',
+        identical:'bg-success',
+        combined:'bg-warning'
+    };
+    $scope.getMergeStatusClass = function(f) {
+        return statusToClass[$scope.mergeStatus[f].status];
+    };
+    
+    $scope.getMergePickerStyle = function(f) {
+        var t = $scope.mergeStatus[f].sourceTab;
+        return `background-color:${t.bgColor};color:${t.fgColor}`;
+    };
+    
+    $scope.getTextForStatus = function(f) {
+        var s = $scope.mergeStatus[f];
+        if(s.status === 'one_empty')  {
+            return `Using value from ${s.source} (other value empty)`;
+        }
+        if(s.status === 'identical') {
+            return 'Identical values';
+        }
+        if(s.status === 'manual_merge_required') {
+            return 'Different values in each record.  Merge Manually';
+        }
+        if(s.status === 'combined') {
+            return 'Automatically combined the values';
+        }
+        
+    };
+    
+    
+    $scope.getDisplayValue = function(t, fieldName) {
+        var val = t.object[fieldName];
+        
+        return val._disp || val;
+    };
+    
+    $scope.pickValue = function(t, fieldName) {
+        $scope.mergedObj[fieldName] = t.object[fieldName]
+        var ms = $scope.mergeStatus[fieldName];
+        ms.sourceTab = t;
+        ms.source = t.index;
+        // console.log(`mergeStatus for ${fieldName}`, ms);
+        
+    };
+    
+    
+    
+    
      
-     $scope.swap = function() {
-         console.log('swap', theDupeList);
-         var a = $scope.objectA;
-         var b = $scope.objectB;
-         
-         for(var i=0; i < theDupeList.dups.length; i++) {
-            //  console.log(theDupeList.dups[i]);
-            if(theDupeList.dups[i]._id === b._id) {
-                theDupeList.dups[i]._id = a._id;
-                break;
-            }
-         }
-         theDupeList.orig._id = b._id;
-         
-        $scope.objectA = b;
-        $scope.objectB = a;
-         
-         theDupeList.save().then(function() {
-             
-             for(var i=0; i < $scope.allObjects.length; i++) {
-                 if($scope.allObjects[i].obj === b) {
-                     $scope.allObjects[i] = {
-                         obj:a,
-                         label:(i+1)+'. '+a._disp+' ('+a._id+')'
-                     };
-                 }
-             }
-             $scope.refreshForms();
-             
-
-         });
-     };
-     
-     $scope.refreshForms = function() {
+    $scope.refreshForms = function() {
          $scope.refreshing = true;
          $timeout(function() {
              $scope.refreshing = false;
-             wiredup = false;
+            //  wiredup = false;
          }, 100);
-     };
+    };
      
-     $scope.objectBChanged = function(objectB) {
-         $scope.objectB = objectB;
-         $scope.refreshForms();
-     };
 }
